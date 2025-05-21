1f6db6ed99ae98f3e92191aad14fde9b3d032337
conny-coder <kostia.shkambula@gmail.com>
2025-05-21 20:36:33 +0300

    little fix

--- src/pending-review/dto/create-review.dto.ts
+++ src/pending-review/dto/create-review.dto.ts
@@ -1,4 +1,4 @@
-import { IsNotEmpty, IsNumber, IsString, Max, Min } from "class-validator";
+import { IsNotEmpty, IsNumber, IsString, Max, Min, MinLength } from "class-validator";
 import { IsObjectId } from "class-validator-mongo-object-id";
 import { Types } from "mongoose";
 
@@ -16,5 +16,6 @@ export class CreateReviewDto {
 
   @IsString({ message: 'Коментар має бути рядком' })
   @IsNotEmpty({ message: 'Коментар не може бути порожнім' })
+  @MinLength(10, { message: 'Коментар повинен бути не менше 10 символів' })
   comment: string;
 }
